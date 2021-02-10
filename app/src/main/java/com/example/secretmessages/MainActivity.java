9c1c83e9e6c210668afa62cd6280657077b9492a
Alexander Tih <dgioto@gmail.com>
2021-02-10 21:22:46 +0200

    Шыфрование русского алфавита

--- app/src/main/java/com/example/secretmessages/MainActivity.java
+++ app/src/main/java/com/example/secretmessages/MainActivity.java
@@ -32,14 +32,14 @@ public class MainActivity extends AppCompatActivity {
         char key = (char) keyVal;
         for (int x = 0; x < message.length(); x++){
             char input = message.charAt(x);
-            if (input >= 'A' && input <= 'Z'){
+            if (input >= 'А' && input <= 'Я'){
                 input += key;
-                if (input > 'Z') input -= 26;
-                if (input < 'A') input += 26;
-            } else if (input>= 'a' && input <= 'z') {
+                if (input > 'Я') input -= 32;
+                if (input < 'А') input += 32;
+            } else if (input>= 'а' && input <= 'я') {
                 input += key;
-                if (input > 'z') input -= 26;
-                if (input < 'a') input += 26;
+                if (input > 'я') input -= 32;
+                if (input < 'а') input += 32;
             } else if (input >= '0' && input <= '9'){
                 input += (keyVal % 10);
                 if (input > '9') input -= 10;
