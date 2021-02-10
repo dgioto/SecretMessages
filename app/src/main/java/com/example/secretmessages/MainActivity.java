b82d545feb43d22051e81f796544eb64b3eb8ebe
Alexander Tih <dgioto@gmail.com>
2021-02-10 16:29:59 +0200

    Темная тема

--- app/src/main/java/com/example/secretmessages/MainActivity.java
+++ app/src/main/java/com/example/secretmessages/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.secretmessages;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
